4a8d234237c3ac06fc258d5633b52746b52115d2
davalarcon <zilentstorm@gmail.com>
2017-08-15 07:15:51 -0500

    update

--- forloop.js
+++ forloop.js
@@ -1,4 +1,33 @@
+console.log('primer')
+
 for (var i = -10; i < 20; i++) {
     console.log(i);
+
+}
+
+console.log('print even numbers 10 - 40 ');
+
+for (var i = 10; i <= 40; i++) {
+    if (i % 2 === 0) {
+        console.log(i);
+    }
+
+}
+
+console.log('print odd numbers from 300 to 333');
+
+for (var i = 301; i <= 333; i++) {
+    if (i % 2 !== 0) {
+        console.log(i);
+    }
+
+}
+
+console.log('print all numbers divisible by 3 and 5 between 5 and 50');
+
+for (var i = 5; i <= 50; i++) {
+    if ((i % 3 === 0) && (i % 5 === 0)) {
+        console.log(i);
+    }
 
 }
